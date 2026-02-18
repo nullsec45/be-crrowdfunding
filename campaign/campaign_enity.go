171fe8b41e39924f1c654ac1d85ed5299767fdd0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-02-18 23:48:39 +0700

    feat(campaign) : campaign entity.

---
+++ campaign/campaign_enity.go
@@ -0,0 +1,28 @@
+package campaign
+
+import "time"
+
+type Campaign struct {
+	ID 				 int
+	UserID			 int
+	Name 			 string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerAmount	 int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type CampaignImage struct {
+	ID int
+	CampaignID int
+	FileName string
+	IsPrimary int
+	CreatedAt  time.Time
+	UpdatedAt time.Time
+
+}
