d81ab4fc610ccfa14ae2318d2f1ca9c8f8cdf2fc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-03-03 22:09:53 +0700

    feat(campaign) : api upload image.

--- handler/campaign_handler.go
+++ handler/campaign_handler.go
@@ -7,6 +7,12 @@ import (
 	"crowdfunding-api/helper"
 	"net/http"
 	"crowdfunding-api/user"
+	"fmt"
+	"path/filepath"
+	"time"
+	// "mime"
+	// "os"
+	
 )
 
 type campaignHandler struct {
@@ -120,4 +126,55 @@ func (h *campaignHandler) UpdateCampaign(c *gin.Context) {
 
 	response := helper.APIResponse("Success to update campaign", http.StatusOK, "Success", campaign.FormatCampaign(updatedCampaign))
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *campaignHandler) UploadCampaignImage(c *gin.Context) {
+    var input campaign.CreateCampaignImageInput
+
+	err := c.ShouldBind(&input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", err)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		data := gin.H{"is_uploaded":false}
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+	extension := filepath.Ext(file.Filename)
+
+
+    path := fmt.Sprintf("images/campaign-%d-%d%s", input.CampaignID, time.Now().Unix(), extension)
+
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded":false}
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	 _, err = h.service.SaveCampaignImage(input, path)
+
+    if err != nil {
+        data := gin.H{"is_uploaded": false}
+        response := helper.APIResponse(fmt.Sprintf("Failed to upload campaign image: %v", err), http.StatusBadRequest, "error", data)
+        c.JSON(http.StatusBadRequest, response)
+        return
+    }
+
+	data := gin.H{"is_uploaded": true}
+    response := helper.APIResponse("Avatar successfully uploaded", http.StatusOK, "success", data)
+    c.JSON(http.StatusOK, response)
 }
